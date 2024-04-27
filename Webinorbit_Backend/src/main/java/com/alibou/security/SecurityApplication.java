766ffc35742c96da57be296d858a740649281998
Anubhavpandey27 <anubhavpandey.bt22cseds@pec.edu.in>
2024-04-28 01:11:23 +0530

    changed frontend url

--- Webinorbit_Backend/src/main/java/com/alibou/security/SecurityApplication.java
+++ Webinorbit_Backend/src/main/java/com/alibou/security/SecurityApplication.java
@@ -3,11 +3,19 @@ package com.alibou.security;
 import com.alibou.security.auth.AuthenticationService;
 import com.alibou.security.auth.RegisterRequest;
 import com.alibou.security.user.Role;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 import static com.alibou.security.user.Role.ADMIN;
 import static com.alibou.security.user.Role.MANAGER;
@@ -45,4 +53,16 @@ public class SecurityApplication {
 			System.out.println("Server is live");
 		};
 	}
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter();
+	}
+
 }
